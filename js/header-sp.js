0c971592a08085545ae13015059d04c60ce06646
shiho9121 <iwase.shiho.9121@gmail.com>
2022-06-11 01:38:27 +0900

    [add] ハンバーガーメニュー矢印など追加

--- js/header-sp.js
+++ js/header-sp.js
@@ -4,8 +4,10 @@ $(function() {
 
         if ($(this).hasClass('c-active')) {
             $('.p-nav-sp__list').addClass('c-active');
+            $('.c-back-dark').show();
         } else {
             $('.p-nav-sp__list').removeClass('c-active');
+            $('.c-back-dark').hide();
         }
     });
 });
