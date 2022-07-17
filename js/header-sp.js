48982e2f1c5cca0e44854c9bdca25a23776d9f9d
shiho9121 <iwase.shiho.9121@gmail.com>
2022-07-18 00:38:55 +0900

    [fix] jsの修正

--- js/header-sp.js
+++ js/header-sp.js
@@ -4,9 +4,11 @@ $(function() {
 
         if ($(this).hasClass('c-active')) {
             $('.p-nav-sp__list').addClass('c-active');
+            $('body').addClass('c-active--fixed-scroll');
             $('.c-back-dark').show();
         } else {
             $('.p-nav-sp__list').removeClass('c-active');
+            $('body').removeClass('c-active--fixed-scroll');
             $('.c-back-dark').hide();
         }
     });
